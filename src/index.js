cedc6d68d0bc5175baabc2fb9608d98ce918ee53
Alex <argentum.tm@gmail.com>
2021-03-10 17:08:00 +0300

    feat: implement max, min and avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
+exports.min = function min(array) {
+    let min = array && array.length ? array[0] : 0;
+    if (array) {
+        for (let index = 1; index < array.length; index++) {
+            if (min > array[index]) {
+                min = array[index];
+            }
+        }
+    }
+    return min;
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    let max = array && array.length ? array[0] : 0;
+    if (array) {
+        for (let index = 1; index < array.length; index++) {
+            if (max < array[index]) {
+                max = array[index];
+            }
+        }
+    }
+    return max;
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    let avg = 0;
+    if (array && array.length) {
+        let sum = 0;
+        for (let index = 0; index < array.length; index++) {
+            sum += array[index];
+        }
+        avg = sum / array.length;
+    }
+    return avg;
+};
